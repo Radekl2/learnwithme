3a04a1088e05a708377375588d63a6e7511b7dc7
Radosław Smyksy <rsmyksy@gmail.com>
2018-05-05 21:18:20 +0200

    upd

---
+++ www/js/database.js
@@ -0,0 +1,150 @@
+var database = firebase.database().ref();
+
+var addNewAnn = function() {
+    goToSite('addAnnoun');
+    $('#tagsNewAnn').val("");
+    $('#dateNewAnn').val("");
+    $('#startTimeNewAnn').val("");
+    $('#endTimeNewAnn').val("");
+    $('#placeNewAnn').val("");
+    $('#descriptionAnn').val("");
+};
+
+var addAnnoun = function() {
+	var tagi = $('#tagsNewAnn');
+	var dateAnn = $('#dateNewAnn');
+	var startTime = $('#startTimeNewAnn');
+	var endTime = $('#endTimeNewAnn');
+	var placeAnn = $('#placeNewAnn');
+	var description = $('#descriptionAnn');
+
+	var dateAdd = formatDate(new Date());
+
+	var tagiS = tagi.val();
+	var dateAnnS = formatDate(new Date(dateAnn.val()));
+	var startTimeS = startTime.val();
+	var endTimeS = endTime.val();
+	var placeAnnS = placeAnn.val();
+	var descriptionS = description.val();
+
+	var validateInfo = $('#newAnnInfo').css("color", "red");
+
+	if (tagiS.length == 0 || dateAnnS.length == 0 ||
+		startTimeS.length == 0 || endTimeS.length == 0 || 
+		placeAnnS.length == 0 || descriptionS.length == 0) {
+		console.log("Nie wszystkie pola są wypełnione!");
+		validateInfo.text("Proszę uzupełnić puste pola!");
+	} else {
+		var announData = {
+			tags: tagiS,
+			date: dateAnnS,
+			startTime: startTimeS,
+			endTime: endTimeS,
+			place: placeAnnS,
+			description: descriptionS,
+			addDate: dateAdd,
+			lastEdit: dateAdd,
+			active: true
+		};
+		
+		var userID = firebase.auth().currentUser.uid;
+
+		var newKey = firebase.database().ref().child('classifieds').push().key;
+		console.log(newKey);
+
+		var shortData = newKey;
+		database.child('/classifieds/' + newKey).set(announData);
+		database.child('/users/' + userID + '/added').push(shortData);
+		goToSite('mainAdd');
+		tagi.text("");
+		dateAnn.text("");
+		startTime.text("");
+		endTime.text("");
+		placeAnn.text("");
+		description.text("");
+		validateInfo.text("");
+	}
+};
+
+function formatDate(date) {
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+
+    var output = ((''+day).length<2 ? '0' : '') + day + '-' +
+        ((''+month).length<2 ? '0' : '') + month +
+        '-' + date.getFullYear();
+
+    return output;
+}
+
+$(document).ready(function(){
+	
+});
+
+function getAllAnn() {
+	var mainCont = $("#mainAll > div[data-role='main']");
+	mainCont.text('');
+	
+	database.child('classifieds/').on("child_added", function(data, prevChildKey) {
+		var newAnn = data.val();
+		mainCont.append("<div class='container'><span class='annKey'>" + data.key + "</span><p class='infoAboutMeeting'>" +
+			newAnn.date + " " + newAnn.startTime + "-" + newAnn.endTime + 
+			"<br>" + newAnn.place + "<br>" + newAnn.tags +  
+			"</p><img src='img/greyBook.png' class='bookic'/><p class='undimg'></p></div>");
+		
+		//console.log(prevChildKey);
+		//console.log('dodano nowe ogłoszenie!');
+		//console.log(data);
+	});
+	goToSite('mainAll');
+}
+
+function getMyAnn() {
+	var mainContAdd = $("#mainAdd > div[data-role='main']");
+	mainContAdd.text('');
+	database.child('users/' + firebase.auth().currentUser.uid + "/added/").on("child_added", function(data, prevChildKey) {
+		console.log(data.val());
+		if (data.val() == null) {
+			mainContAdd.text("Nic tu nie ma :(");
+		} else {
+			var announ = data.val();
+			
+			database.child('classifieds/' + announ).once("value").then(function(snapshot) {
+				//console.log(snapshot.val());
+				var newAnn = snapshot.val();
+				mainContAdd.append("<div class='container'><span class='annKey'>" + announ + "</span><p class='infoAboutMeeting'>" +
+					newAnn.date + " " + newAnn.startTime + "-" + newAnn.endTime + 
+					"<br>" + newAnn.place + "<br>" + newAnn.tags +  
+					"</p><img src='img/greyBook.png' class='bookic'/><p class='undimg'></p></div>");
+
+				console.log('dodano nowe ogłoszenie!');
+			});
+		}
+	});
+	goToSite('mainAdd');
+}
+
+function getMyWatched() {
+	var mainContWatch = $("#mainWatched > div[data-role='main']");
+	mainContWatch.text('');
+	database.child('users/' + firebase.auth().currentUser.uid + "/watched/").on("child_added", function(data, prevChildKey){
+		console.log(data.val());
+		if (data.val() == null) {
+			mainContAdd.text("Nic tu nie ma :(");
+		} else {
+			var announ = data.val();
+			
+			database.child('classifieds/' + announ).once("value").then(function(snapshot) {
+				//console.log(snapshot.val());
+				var newAnn = snapshot.val();
+				mainContAdd.append("<div class='container'><span class='annKey'>" + announ + "</span><p class='infoAboutMeeting'>" +
+					newAnn.date + " " + newAnn.startTime + "-" + newAnn.endTime + 
+					"<br>" + newAnn.place + "<br>" + newAnn.tags +  
+					"</p><img src='img/greyBook.png' class='bookic'/><p class='undimg'></p></div>");
+
+				console.log('dodano nowe ogłoszenie!');				
+			});
+		}
+	});
+	goToSite('mainWatched');
+}
---
+++ www/js/script.js
@@ -0,0 +1,172 @@
+function goToSite(id){  
+	var anch = document.createElement('a');
+	anch.setAttribute("href", "#"+id);
+	anch.click();
+}
+
+(function() {
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyC2E9hI9X2e2KCHyEiaz5vcaFXOFvY0HQc",
+		authDomain: "learnwithme-98129.firebaseapp.com",
+		databaseURL: "https://learnwithme-98129.firebaseio.com",
+		projectId: "learnwithme-98129",
+		storageBucket: "learnwithme-98129.appspot.com",
+		messagingSenderId: "1032899670271"
+	};
+	firebase.initializeApp(config);
+}());
+
+$(document).ready(function(){
+	const emailInput = document.getElementById('email');
+	const passInput = document.getElementById('haslo');
+	const btnLogIn = document.getElementById('zaloguj');
+	const btnFBLogIn = document.getElementById("zalogujFB");
+	const btnGLogIn = document.getElementById("zalogujG");
+	const btnRegis = document.getElementById("zarejestruj");
+	const continueBtn = document.getElementById("przejdzDoSerwisu");
+	const logout = document.getElementById("wyloguj");
+
+	//EVENT LISTENER DO LOGOWANIA
+	btnLogIn.addEventListener('click', function() {
+		const email = emailInput.value;
+		const passwd = passInput.value;
+		const auth = firebase.auth();
+		const promise = auth.signInWithEmailAndPassword(email, passwd);
+		var info = document.getElementById('logInfo');
+		info.style.color = "red";
+		promise.catch(e => {
+			console.log(e.code);
+			switch (e.code){
+  				case "auth/invalid-email":
+  					info.innerText = "Niepoprawny email!";
+  					emailInput.style.border = "3px solid #ff7777";  					
+  					console.log(e.code);
+  					break;
+  				case "auth/user-not-found":
+  					info.innerText = "Niepoprawny email!";
+  					emailInput.style.border = "3px solid #ff7777";  					
+  					console.log(e.code);
+  					break;  				
+  				case "auth/email-already-in-use":
+  					info.innerText = "Email jest już w użyciu!";
+  					emailInput.style.border = "3px solid #ff7777";  
+  					console.log(e.code);
+  					break;
+  				case "auth/wrong-password":
+  					info.innerText = "Niepoprawne hasło!";
+  					passInput.style.border = "3px solid #ff7777";
+  					console.log(e.code);
+  					break;
+  				default:
+    				info.innerText = "Nieokreślony błąd.";
+    				console.log(e.code);
+    				break;
+  			}
+		});
+		info.innerText = "";
+		passInput.style.border = "0px solid #ff7777";
+		emailInput.style.border = "0px solid #ff7777";
+		getAllAnn();
+	});
+
+	//EVENT LISTENER DO LOGOWANIA PRZEZ FB
+	btnFBLogIn.addEventListener('click', function() {
+
+	});
+
+	//EVENT LISTENER DO LOGOWANIA PRZEZ GOOGLE
+	btnGLogIn.addEventListener('click', function() {
+
+	});
+
+	//EVENT LISTENER DO REJESTRACJI
+	btnRegis.addEventListener('click', function() {
+		const nickReg = document.getElementById('nickreg').value;
+		const emailRegInput = document.getElementById('emailreg');
+		const emailReg = emailRegInput.value;
+		const passwdReg1Input = document.getElementById('hasloreg');
+		const passwdReg1 = passwdReg1Input.value;
+		const passwdReg2Input = document.getElementById('haslopowtreg');
+		const passwdReg2 = passwdReg2Input.value;
+		var info = document.getElementById('regInfo');
+		info.style.color = "red";
+		if (passwdReg1 === passwdReg2) {
+			const auth = firebase.auth();
+			const promise = auth.createUserWithEmailAndPassword(emailReg, passwdReg1);
+			promise.catch(e => {
+				switch (e.code){
+					case "auth/invalid-email":
+						info.innerText = "Niepoprawny email!";
+						emailRegInput.style.border = "3px solid #ff7777";
+						console.log(e.code);
+						break;
+					case "auth/weak-password":
+						info.innerText = "Hasło musi zawierać co najlniej 6 znaków!";
+						passwdReg1Input.style.border = "3px solid #ff7777";
+						passwdReg2Input.style.border = "3px solid #ff7777";
+						console.log(e.code);
+						break;
+					case "auth/email-already-in-use":
+						info.innerText = "Email jest już w użyciu!";
+						emailRegInput.style.border = "3px solid #ff7777";
+						passwdReg1Input.style.border = "0px solid #ff7777";
+						passwdReg2Input.style.border = "0px solid #ff7777";
+						console.log(e.code);
+						break;
+					default:
+						info.innerText = "Nieokreślony błąd.";
+						console.log(e.code);
+						break;
+				}
+			});
+			promise.then(function(){
+				info.innerText = "";
+				emailRegInput.style.border = "0px solid #ff7777";
+				passwdReg1Input.style.border = "0px solid #ff7777";
+				passwdReg2Input.style.border = "0px solid #ff7777";
+				addUserToDB(emailReg, nickReg, firebase.auth().currentUser.uid);
+				goToSite('afterRegPage');
+			});
+		} else {
+			console.log("Hasła są niepoprawne");
+			info.innerText = "Hasła nie są identyczne!";
+			passwdReg1Input.style.border = "3px solid #ff7777";
+			passwdReg2Input.style.border = "3px solid #ff7777";
+		}
+	});
+
+    //EVENT LISTENER DO PRZEJSCIA DO SERWISU
+	continueBtn.addEventListener('click', function() {
+        getAllAnn();
+    });
+
+	//EVENT LISTENER ZMIANY STATUSU ZALOGOWANIA
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if (firebaseUser) {
+			console.log(firebaseUser);
+		} else {
+			console.log("User not logged in");
+		}
+	});
+
+    //EVENT LISTENER DO WYLOGOWANIA
+	logout.addEventListener('click', function() {
+
+	});
+});
+/*
+var someDiv = document.getElementById('someDiv');
+  var dbRef = firebase.database().ref().child('text');
+  dbRef.on('value', snap => someDiv.innerText = snap.val());
+*/
+function addUserToDB(email, nick, userKey){
+	var emails = email.replace('.', ',');
+	var userData = {
+		email: emails,
+		name: nick,
+		watched: [],
+		added: []
+	};
+	firebase.database().ref().child('users/' + userKey).set(userData);
+}
