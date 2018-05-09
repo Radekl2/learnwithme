4d31e4605e53811827ded2f2af01baec3ca3edc9
Radosław Smyksy <rsmyksy@gmail.com>
2018-05-09 22:19:35 +0200

    upd

--- www/js/script.js
+++ www/js/script.js
@@ -230,50 +230,15 @@ function chartClick(){
   });
 }
 
-(function (global) {
-    var DemoViewModel,
-        app = global.app = global.app || {};
-
-    DemoViewModel = kendo.data.ObservableObject.extend({
-
-        checkAvailable: function () {
-            if (!this.checkSimulator()) {
-              cordova.plugins.email.isAvailable(this.callback);
-            }
-        },
-
-        composeEmail: function () {
-            if (!this.checkSimulator()) {
-                cordova.plugins.email.open({
-                    to:          ['person1@domain.com'],
-                    cc:          ['person2@domain.com'],
-                    bcc:         ['person3@domain.com', 'person4@domain.com'],
-                    attachments: ['file://styles/images/logo.png', 'file://styles/images/logo2x.png'],
-                    subject:     'EmailComposer plugin test',
-                    body:        '<h2>Hello!</h2>This is a nice <strong>HTML</strong> email with two attachments.',
-                    isHtml:      true
-                }, this.callback)
-            }
-        },
-
-        callback: function(msg) {
-            navigator.notification.alert(JSON.stringify(msg), null, 'EmailComposer callback', 'Close');
-        },
-
-        checkSimulator: function() {
-            if (window.navigator.simulator === true) {
-                alert('This plugin is not available in the simulator.');
-                return true;
-            } else if (window.cordova === undefined || window.cordova.plugins === undefined) {
-                alert('Plugin not found. Maybe you are running in AppBuilder Companion app which currently does not support this plugin.');
-                return true;
-            } else {
-                return false;
-            }
-        }
-    });
-
-    app.demoService = {
-        viewModel: new DemoViewModel()
-    };
-})(window);
+function send()
+{
+  document.addEventListener('deviceready', function () {
+   cordova.plugins.email.open({
+    to:      'max@mustermann.de',
+    cc:      'erika@mustermann.de',
+    bcc:     ['john@doe.com', 'jane@doe.com'],
+    subject: 'Greetings',
+    body:    'How are you? Nice greetings from Leipzig'
+});
+}, false);
+}
